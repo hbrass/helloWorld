fa759ef3944176b45dbf22607fb11cde7e03a64a
Harry-Thaine Brass <p14179109@myemail.dmu.ac.uk>
2016-02-10 17:07:25 +0000

    git add 10/02/2016

---
+++ maths_branching/maths_function.c
@@ -0,0 +1,38 @@
+//This is the line causing issue in the master branch
+#include <stdio.h>
+
+
+/* addition */
+
+float add (float x, float y) {
+	
+	return x + y;
+}
+
+/*subtraction*/
+
+float sub (float x, float y) {
+
+        return x - y;
+}
+
+/* multiplication*/
+
+float mult (float x, float y) {
+
+        return x * y;
+}
+
+/* division */
+
+float div (float x, float y) {
+
+        return x / y;
+}
+
+/* remainder */
+
+float rem (float x, float y) {
+
+	return x % y;
+}
