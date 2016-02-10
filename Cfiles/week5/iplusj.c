05e40e0df8eeccd51d36de885a507e6bc1048ff3
Harry-Thaine Brass <p14179109@myemail.dmu.ac.uk>
2016-02-10 17:22:37 +0000

    10/02/2016 17:22

---
+++ Cfiles/week5/iplusj.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, k;
+    
+    
+    printf("enter value for I: ");
+    scanf(" %i", &i);
+    printf("enter value for J: ");
+    scanf(" %i", &j);
+    
+     k = i + j;
+    
+    printf("i   = %i\n", i);
+    printf("j   = %i\n", j);
+    printf("k   = %i\n", k);
+    printf("i+j = %i\n", i+j);
+    return 0;
+}
