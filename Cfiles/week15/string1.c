c44463e5dfee60feda8ef7111c98098389e8d886
Harry-Thaine Brass <p14179109@myemail.dmu.ac.uk>
2016-02-10 17:30:10 +0000

    10/02/2016 17:30

---
+++ Cfiles/week15/string1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	char str [79];  //sets the max st
+
+	
+	printf("please enter a string: ");
+	gets(str);
+	printf("%s\n",str);
+	  
+
+}
---
+++ Cfiles/week15/string2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+
+
+int main()
+
+	char str [79];  //sets the max st
+	int;
+	
+	printf("please enter a string: ");
+	gets(str);
+	
+	for (i = 0; i < length; i++
+	
+	printf("%s\n",str);
+	  
+	  
+
+}
